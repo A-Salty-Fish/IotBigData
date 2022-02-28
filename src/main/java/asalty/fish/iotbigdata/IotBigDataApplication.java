5a1f424ac62c82800c45a01426fc4ee02c818e7d
A-Salty-Fish <1309032417@qq.com>
2022-02-28 14:38:11 +0800

    feat: connect to clickhouse

--- src/main/java/asalty/fish/iotbigdata/IotBigDataApplication.java
+++ src/main/java/asalty/fish/iotbigdata/IotBigDataApplication.java
@@ -1,6 +1,7 @@
 package asalty.fish.iotbigdata;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
--- src/test/java/asalty/fish/iotbigdata/IotBigDataApplicationTests.java
+++ src/test/java/asalty/fish/iotbigdata/IotBigDataApplicationTests.java
@@ -1,13 +1,34 @@
 package asalty.fish.iotbigdata;
 
+import asalty.fish.iotbigdata.config.ClickHouseConfig;
+import com.google.gson.Gson;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+import java.sql.*;
 
 @SpringBootTest
 class IotBigDataApplicationTests {
 
+    @Resource
+    Statement clickHouseStatement;
+
     @Test
     void contextLoads() {
+
+    }
+
+    @Test
+    public void testClickHouse() {
+        try {
+            ResultSet rs = clickHouseStatement.executeQuery("select count(*) from tutorial.hits_v1");
+            while (rs.next()) {
+                System.out.println(rs.getString(1));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
