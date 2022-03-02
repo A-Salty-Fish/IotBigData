098a49e90d890b5bb9dc52c2f8ebc987f3d4ab65
A-Salty-Fish <1309032417@qq.com>
2022-03-02 19:37:53 +0800

    refactor：移出无用代码

--- src/test/java/asalty/fish/iotbigdata/IotBigDataApplicationTests.java
+++ src/test/java/asalty/fish/iotbigdata/IotBigDataApplicationTests.java
@@ -1,7 +1,5 @@
 package asalty.fish.iotbigdata;
 
-import asalty.fish.iotbigdata.clickhouseJpa.ClickHouseMapper;
-import asalty.fish.iotbigdata.mapper.entity.hits_v1;
 import com.google.gson.Gson;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,26 +11,9 @@ import java.util.List;
 @SpringBootTest
 class IotBigDataApplicationTests {
 
-    @Resource
-    Statement clickHouseStatement;
-
-    @Resource
-    ClickHouseMapper clickHouseDao;
-
     @Test
     void contextLoads() {
 
-    }
-
-    @Test
-    public void testClickHouse() {
-        try {
-            ResultSet rs = clickHouseStatement.executeQuery("select * from tutorial.hits_v1 limit 2");
-            List<hits_v1> list = clickHouseDao.convertResultSetToList(rs, hits_v1.class);
-            System.out.println(new Gson().toJson(list));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 
 }
