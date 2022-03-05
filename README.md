dcdc57969a0e356bf987e8c4bd02454bb76471ae
A-Salty-Fish <1309032417@qq.com>
2022-03-05 10:10:33 +0800

    feat: 不同批量写入的benchmark

---
+++ README.md
@@ -0,0 +1,9 @@
+# 物联网时空大数据存储与索引
+本科毕设
+
+## 数据接口层
+1. clickhouse-jpa
+
+### 编码模块
+1. GeoHash （Google S2）
+
--- src/main/java/asalty/fish/iotbigdata/benchmark/BatchInsertBenchMark.java
+++ src/main/java/asalty/fish/iotbigdata/benchmark/BatchInsertBenchMark.java
@@ -114,6 +114,17 @@ public class BatchInsertBenchMark {
     }
 
     public static void main(String[] args) throws Exception {
+        // 生成测试函数
+//        for (int i = 1; i <= 30 ;i++) {
+//
+//            System.out.println(
+//                    "@Benchmark\n" +
+//                            "    public void testClickHouseBatchInsertBy"+ (10  * i) + "() {\n" +
+//                            "        testCreateTableDao.batchCreate(getTestCreateEntitys(" + (10 *i) + "));\n" +
+//                            "    }"
+//            );
+//
+//        }
         Options options = new OptionsBuilder()
                 .include(BatchInsertBenchMark.class.getSimpleName())
                 .resultFormat(ResultFormatType.JSON)
@@ -151,20 +162,136 @@ public class BatchInsertBenchMark {
 //    public void testMysqlBatchInsertBy100() {
 //        testMysqlTableDao.saveAll(getInsertTestMysqlTables(100));
 //    }
+
+//    @Benchmark
+//    public void testMysqlBatchInsertBy200() {
+//        testMysqlTableDao.saveAll(getInsertTestMysqlTables(200));
+//    }
 //
 //    @Benchmark
-//    public void testClickHouseBatchInsertBy100() {
-//        testCreateTableDao.batchCreate(getTestCreateEntitys(100));
+//    public void testClickHouseBatchInsertBy200() {
+//        testCreateTableDao.batchCreate(getTestCreateEntitys(200));
 //    }
 
     @Benchmark
-    public void testMysqlBatchInsertBy200() {
-        testMysqlTableDao.saveAll(getInsertTestMysqlTables(200));
+    public void testClickHouseBatchInsertBy10() {
+        testCreateTableDao.batchCreate(getTestCreateEntitys(10));
     }
-
+    @Benchmark
+    public void testClickHouseBatchInsertBy20() {
+        testCreateTableDao.batchCreate(getTestCreateEntitys(20));
+    }
+    @Benchmark
+    public void testClickHouseBatchInsertBy30() {
+        testCreateTableDao.batchCreate(getTestCreateEntitys(30));
+    }
+    @Benchmark
+    public void testClickHouseBatchInsertBy40() {
+        testCreateTableDao.batchCreate(getTestCreateEntitys(40));
+    }
+    @Benchmark
+    public void testClickHouseBatchInsertBy50() {
+        testCreateTableDao.batchCreate(getTestCreateEntitys(50));
+    }
+    @Benchmark
+    public void testClickHouseBatchInsertBy60() {
+        testCreateTableDao.batchCreate(getTestCreateEntitys(60));
+    }
+    @Benchmark
+    public void testClickHouseBatchInsertBy70() {
+        testCreateTableDao.batchCreate(getTestCreateEntitys(70));
+    }
+    @Benchmark
+    public void testClickHouseBatchInsertBy80() {
+        testCreateTableDao.batchCreate(getTestCreateEntitys(80));
+    }
+    @Benchmark
+    public void testClickHouseBatchInsertBy90() {
+        testCreateTableDao.batchCreate(getTestCreateEntitys(90));
+    }
+    @Benchmark
+    public void testClickHouseBatchInsertBy100() {
+        testCreateTableDao.batchCreate(getTestCreateEntitys(100));
+    }
+    @Benchmark
+    public void testClickHouseBatchInsertBy110() {
+        testCreateTableDao.batchCreate(getTestCreateEntitys(110));
+    }
+    @Benchmark
+    public void testClickHouseBatchInsertBy120() {
+        testCreateTableDao.batchCreate(getTestCreateEntitys(120));
+    }
+    @Benchmark
+    public void testClickHouseBatchInsertBy130() {
+        testCreateTableDao.batchCreate(getTestCreateEntitys(130));
+    }
+    @Benchmark
+    public void testClickHouseBatchInsertBy140() {
+        testCreateTableDao.batchCreate(getTestCreateEntitys(140));
+    }
+    @Benchmark
+    public void testClickHouseBatchInsertBy150() {
+        testCreateTableDao.batchCreate(getTestCreateEntitys(150));
+    }
+    @Benchmark
+    public void testClickHouseBatchInsertBy160() {
+        testCreateTableDao.batchCreate(getTestCreateEntitys(160));
+    }
+    @Benchmark
+    public void testClickHouseBatchInsertBy170() {
+        testCreateTableDao.batchCreate(getTestCreateEntitys(170));
+    }
+    @Benchmark
+    public void testClickHouseBatchInsertBy180() {
+        testCreateTableDao.batchCreate(getTestCreateEntitys(180));
+    }
+    @Benchmark
+    public void testClickHouseBatchInsertBy190() {
+        testCreateTableDao.batchCreate(getTestCreateEntitys(190));
+    }
     @Benchmark
     public void testClickHouseBatchInsertBy200() {
         testCreateTableDao.batchCreate(getTestCreateEntitys(200));
+    }
+    @Benchmark
+    public void testClickHouseBatchInsertBy210() {
+        testCreateTableDao.batchCreate(getTestCreateEntitys(210));
+    }
+    @Benchmark
+    public void testClickHouseBatchInsertBy220() {
+        testCreateTableDao.batchCreate(getTestCreateEntitys(220));
+    }
+    @Benchmark
+    public void testClickHouseBatchInsertBy230() {
+        testCreateTableDao.batchCreate(getTestCreateEntitys(230));
+    }
+    @Benchmark
+    public void testClickHouseBatchInsertBy240() {
+        testCreateTableDao.batchCreate(getTestCreateEntitys(240));
+    }
+    @Benchmark
+    public void testClickHouseBatchInsertBy250() {
+        testCreateTableDao.batchCreate(getTestCreateEntitys(250));
+    }
+    @Benchmark
+    public void testClickHouseBatchInsertBy260() {
+        testCreateTableDao.batchCreate(getTestCreateEntitys(260));
+    }
+    @Benchmark
+    public void testClickHouseBatchInsertBy270() {
+        testCreateTableDao.batchCreate(getTestCreateEntitys(270));
+    }
+    @Benchmark
+    public void testClickHouseBatchInsertBy280() {
+        testCreateTableDao.batchCreate(getTestCreateEntitys(280));
+    }
+    @Benchmark
+    public void testClickHouseBatchInsertBy290() {
+        testCreateTableDao.batchCreate(getTestCreateEntitys(290));
+    }
+    @Benchmark
+    public void testClickHouseBatchInsertBy300() {
+        testCreateTableDao.batchCreate(getTestCreateEntitys(300));
     }
 
     @TearDown
