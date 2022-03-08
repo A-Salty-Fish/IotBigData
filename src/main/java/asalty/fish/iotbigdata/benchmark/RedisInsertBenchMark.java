c99b443675a16626191a2bebb5139e06c7793843
A-Salty-Fish <1309032417@qq.com>
2022-03-08 14:25:01 +0800

    feat: redis benchmark

---
+++ src/main/java/asalty/fish/iotbigdata/benchmark/RedisInsertBenchMark.java
@@ -0,0 +1,84 @@
+package asalty.fish.iotbigdata.benchmark;
+
+import asalty.fish.iotbigdata.IotBigDataApplication;
+import asalty.fish.iotbigdata.entity.TestCreateTable;
+import com.google.gson.Gson;
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.results.format.ResultFormatType;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+import org.openjdk.jmh.runner.options.TimeValue;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author 13090
+ * @version 1.0
+ * @description: TODO
+ * @date 2022/3/8 14:18
+ */
+@BenchmarkMode(Mode.Throughput)
+@OutputTimeUnit(TimeUnit.SECONDS)
+@State(Scope.Benchmark)
+public class RedisInsertBenchMark {
+
+    private ConfigurableApplicationContext context;
+
+    private RedisTemplate<String, String> redisTemplate;
+
+    ThreadLocal<Gson> gson = ThreadLocal.withInitial(Gson::new);
+
+    public TestCreateTable getTestCreateEntity() {
+        TestCreateTable h = new TestCreateTable();
+        Random random = new Random();
+        h.setId((long) random.nextInt(1000000000));
+        h.setGoodEvent("goodEvent" + random.nextInt(100000));
+        h.setJavaEnable(random.nextBoolean());
+        h.setTitle("title" + random.nextInt(100000));
+        h.setWatchID((long) random.nextInt(1000000000));
+        h.setUserAgentMajor(random.nextInt(100000));
+        h.setTestUserDefinedColumn("testUserDefinedColumn" + random.nextInt(100000));
+        h.setCreateDay(LocalDate.now());
+        h.setCreateTime(LocalDateTime.now());
+        return h;
+    }
+
+    @Setup(Level.Trial)
+    public void init() throws Exception {
+        context = SpringApplication.run(IotBigDataApplication.class);
+        redisTemplate = context.getBean("redisTemplate", RedisTemplate.class);
+    }
+
+    @TearDown
+    public void tearDown() {
+        context.close();
+    }
+
+    @Benchmark
+    public void testRedisInsert() {
+        TestCreateTable h = getTestCreateEntity();
+        redisTemplate.opsForValue().set(h.getId().toString(), gson.get().toJson(h));
+    }
+
+    public static void main(String[] args) throws Exception {
+        Options options = new OptionsBuilder()
+                .include(RedisInsertBenchMark.class.getSimpleName())
+                .resultFormat(ResultFormatType.JSON)
+                .measurementIterations(1)
+                .measurementTime(TimeValue.seconds(5))
+                .threads(12)
+                .warmupForks(0)
+                .warmupIterations(0)
+                .forks(1)
+                .build();
+        new Runner(options).run();
+    }
+
+}
