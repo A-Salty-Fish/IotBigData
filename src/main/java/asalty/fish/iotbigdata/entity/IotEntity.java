905f852d8c65a5a4dc4ac0de49345d6a008a7ed6
A-Salty-Fish <1309032417@qq.com>
2022-03-10 15:06:25 +0800

    feat: iot 实体

---
+++ src/main/java/asalty/fish/iotbigdata/entity/IotEntity.java
@@ -0,0 +1,32 @@
+package asalty.fish.iotbigdata.entity;
+
+import asalty.fish.clickhousejpa.annotation.ClickHouseColumn;
+import asalty.fish.clickhousejpa.annotation.ClickHouseEngine;
+import asalty.fish.clickhousejpa.annotation.ClickHouseEntity;
+import asalty.fish.clickhousejpa.annotation.ClickHouseTable;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+/**
+ * @author 13090
+ * @version 1.0
+ * @description: TODO
+ * @date 2022/3/10 14:51
+ */
+@ClickHouseEntity
+@Data
+@ClickHouseTable(name = "iot_entity", engine = ClickHouseEngine.MergeTree)
+public class IotEntity {
+    // 设备编号
+    @ClickHouseColumn(isPrimaryKey = true)
+    private Long DeviceId;
+    // 设备属性名
+    private String PropertyName;
+    // 设备属性值
+    private Long Value;
+    // 上报时间
+    private LocalDateTime UpdateDateTime;
+    // 设备地理位置
+    private Long GeoHash;
+}
